58d5c08a91bde2a5bf0ecb9c6532d54a4d046b21
Marwan Ahmed <155799754+marawan206@users.noreply.github.com>
2024-10-08 23:47:48 +0300

    updated v1

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
